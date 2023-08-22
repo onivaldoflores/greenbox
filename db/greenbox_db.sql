456ea11f000775528b21aa9706a2ad0df5377a07
onivaldoflores <onivaldo.junior@ifpr.edu.br>
2023-08-22 14:46:10 -0300

    Arquivos atualizados para a aula do dia 25/08

--- db/greenbox_db.sql
+++ db/greenbox_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 07-Ago-2023 às 16:33
+-- Tempo de geração: 22-Ago-2023 às 17:45
 -- Versão do servidor: 8.0.27
 -- versão do PHP: 7.4.26
 
@@ -44,11 +44,25 @@ CREATE TABLE IF NOT EXISTS `categoria` (
 
 DROP TABLE IF EXISTS `fotos`;
 CREATE TABLE IF NOT EXISTS `fotos` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `id_noticia` int NOT NULL,
+  `foto` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `legenda` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `fotos`
+--
+
+INSERT INTO `fotos` (`id`, `id_noticia`, `foto`, `legenda`) VALUES
+(1, 13, 'qrcode_suap.ifpr.edu.br.png', 'teste'),
+(2, 14, '', 'teste'),
+(3, 15, '', 'teste'),
+(4, 16, '', 'teste'),
+(5, 17, '', 'teste'),
+(6, 18, 'Array', 'teste'),
+(7, 19, 'Array', 'teste');
 
 -- --------------------------------------------------------
 
@@ -76,11 +90,41 @@ CREATE TABLE IF NOT EXISTS `lote` (
 
 DROP TABLE IF EXISTS `noticias`;
 CREATE TABLE IF NOT EXISTS `noticias` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `titulo` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `texto` blob NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `noticias`
+--
+
+INSERT INTO `noticias` (`id`, `titulo`, `texto`) VALUES
+(1, 'titulo', 0x746578746f),
+(2, 'titulo1', 0x746578746f31),
+(3, 'asd', 0x617364),
+(4, 'asd', 0x617364),
+(5, 'teste', 0x7465737465),
+(6, 'teste', 0x7465737465),
+(7, 'teste', 0x7465737465),
+(8, 'teste', 0x7465737465),
+(9, 'teste', 0x7465737465),
+(10, 'teste', 0x7465737465),
+(11, 'teste', 0x7465737465),
+(12, 'teste', 0x7465737465),
+(13, 'teste', 0x7465737465),
+(14, 'teste', 0x7465737465),
+(15, 'teste', 0x7465737465),
+(16, 'teste', 0x7465737465),
+(17, 'teste', 0x7465737465),
+(18, 'teste', 0x7465737465),
+(19, 'teste', 0x7465737465),
+(20, 'teste', 0x7465737465),
+(21, 'teste', 0x7465737465),
+(22, 'teste', 0x7465737465),
+(23, 'teste', 0x7465737465),
+(24, 'teste', 0x7465737465);
 
 -- --------------------------------------------------------
 
@@ -133,7 +177,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `email` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `senha` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `usuarios`
@@ -145,7 +189,9 @@ INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
 (3, 'teste1', 'teste1', 'teste1'),
 (4, 'asd', 'asd', 'asd'),
 (5, '', 'onivaldo.junior', '123456'),
-(6, 'teste', 'teste', '123456');
+(6, 'teste', 'teste', '123456'),
+(7, 'test5', 'test5', 'test5'),
+(8, 'user1', 'user1', 'user1');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
